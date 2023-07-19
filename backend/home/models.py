ae4e6370ad65a08bc1f9beee9fc24daa0c8dd68a
crowdbotics-dev <engineering@crowdbotics.com>
2023-07-19 16:58:09 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -5,6 +5,6 @@ class Vferw(models.Model):
     bddh12 = models.BigIntegerField(blank=True,)
 class Cswd(models.Model):
     'Generated Model'
-    dsfsf89 = models.BigIntegerField(blank=True,null=True,)
+    dsfsf89 = models.BigIntegerField(blank=True,)
     xvxbxb = models.BigIntegerField(blank=True,)
-    bxbx = models.BigIntegerField(blank=True,)
+    bxb = models.BigIntegerField(blank=True,null=True,)
