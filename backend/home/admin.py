c7c8854c59a7931adf7a4f9021e6384d12ba270f
crowdbotics-dev <engineering@crowdbotics.com>
2023-07-19 16:43:29 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Vferw
+admin.site.register(Vferw)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,5 @@
+from django.conf import settings
 from django.db import models
+class Vferw(models.Model):
+    'Generated Model'
+    bddh = models.BigIntegerField()
